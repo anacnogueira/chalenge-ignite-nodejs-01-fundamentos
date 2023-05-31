14103debce85b1c7db39625aceece86e6566b42a
Ana Claudia <anacnogueira@gmail.com>
2023-05-31 20:46:19 -0300

    Lista tasks

--- src/routes.js
+++ src/routes.js
@@ -24,4 +24,13 @@ export const routes = [
       return res.writeHead(201).end();
     },
   },
+  {
+    method: "GET",
+    path: "/tasks",
+    handler: (req, res) => {
+      const tasks = database.select("tasks");
+
+      return res.end(JSON.stringify(tasks));
+    },
+  },
 ];
