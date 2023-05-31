56fc0c87edb4fe283dd0bd52e58b80649ef91307
Ana Claudia <anacnogueira@gmail.com>
2023-05-31 20:41:00 -0300

    Adiciona nova task na tabela

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,26 @@
+import { randomUUID } from "node:crypto";
+import { Database } from "./database.js";
+
+const database = new Database();
+
 export const routes = [
   {
     method: "POST",
     path: "/tasks",
     handler: (req, res) => {
+      const { title, description } = req.body;
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date(),
+      };
+
+      database.insert("tasks", task);
+
       return res.writeHead(201).end();
     },
   },
